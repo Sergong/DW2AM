520c5a562688aa28f67a080afafd284d39eed680
Serge Meeuwsen <serge.meeuwsen@gmail.com>
2016-01-19 00:46:01 +0100

    Commit 11

--- import-dw-to-am.py
+++ import-dw-to-am.py
@@ -11,5 +11,5 @@ os.system('python3 retrieve-identifiers.py')
 os.system('python3 insert-songs.py')
 
 WEEKLY_PLAYLIST_NAME = "Discover_Weekly_{}".format("_".join(str(v) for v in datetime.date.today().isocalendar()[:2]))
-print("Executing: python3 itunes.py new_playlist " + WEEKLY_PLAYLIST_NAME)
+print("Executing: python3 itunes-playlist.py " + WEEKLY_PLAYLIST_NAME)
 os.system("python3 itunes-playlist.py " + WEEKLY_PLAYLIST_NAME)
