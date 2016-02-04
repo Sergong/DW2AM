e4f13ecc82d26c5ddd2671514c348596bd263cf5
Serge Meeuwsen <serge.meeuwsen@gmail.com>
2016-02-04 20:21:26 +0100

    Commit 13

--- import-dw-to-am.py
+++ import-dw-to-am.py
@@ -7,9 +7,14 @@ import os
 import datetime
 
 os.system('python3 export-dw-playlist.py')
-os.system('python3 retrieve-identifiers.py')
-os.system('python3 insert-songs.py')
 
-WEEKLY_PLAYLIST_NAME = "Discover_Weekly_{}".format("_".join(str(v) for v in datetime.date.today().isocalendar()[:2]))
-print("Executing: python3 itunes-playlist.py " + WEEKLY_PLAYLIST_NAME)
-os.system("python3 itunes-playlist.py " + WEEKLY_PLAYLIST_NAME)
+response = input("Would you like to import this playlist? [Y]es or any other key for No: ")
+
+if response.upper() == "Y":
+    os.system('python3 retrieve-identifiers.py')
+    os.system('python3 insert-songs.py')
+
+    WEEKLY_PLAYLIST_NAME = "Discover_Weekly_{}".format("_".join(str(v) for v in datetime.date.today().isocalendar()[:2]))
+    print("Executing: python3 itunes-playlist.py " + WEEKLY_PLAYLIST_NAME)
+    os.system("python3 itunes-playlist.py " + WEEKLY_PLAYLIST_NAME)
+
